c250439d770670a2a61d88ffcd2b26a5110280ca
FlorPiq <104107420+FlorPiq@users.noreply.github.com>
2022-04-25 21:07:27 -0300

    Initial commit

---
+++ src/LaGranjera2.ts
@@ -0,0 +1,8 @@
+console.log("avanzar 1");
+console.log("llenar 1");
+console.log("avanzar 3");
+console.log("llenar 1");
+console.log("avanzar 1");
+console.log("quitar 1");
+console.log("avanzar 1");
+console.log("quitar 1");
---
+++ src/ejercicio1.ts
@@ -0,0 +1,6 @@
+console.log("Crear un posteo en facebook");
+
+console.log("Escribir el texto");
+console.log("Subir una imagen");
+console.log("etiquetar amigos");
+console.log("clickear botón subir");
